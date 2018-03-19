31ac9be35adb5ee9967cd1fe43a0a0ccdcf7ae59
gustavo2011 <gustavo.jauregui2011@gmail.com>
2018-03-18 21:43:05 -0700

    js correctly working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,14 @@ console.log("you gotta guess this number " + randomNumber);
 //displays number on html
 $("#numberToGuess").html(randomNumber);
 
+//variables equal zero
 var wins = 0;
 var losses = 0;
-var userTotal =0;
+var userTotal = 0;
+
+$("#totalScore").html(userTotal);
+$("#wins").html(wins);
+$("#losses").html (losses);
 
 //generates random number for crystals
 var pinkCrystal = 1 + Math.floor(Math.random() * 12);
@@ -22,15 +27,52 @@ console.log("orange crystal number" + orangeCrystal);
 var blackCrystal = 1 + Math.floor(Math.random() * 12);
 console.log("black crystal number" + blackCrystal); 
 
+
+
+// resets game when user wins or lose
+var reset = function() {
+    userTotal= 0;
+
+var randomNumber = 19 + Math.floor(Math.random()* 120);
+    var pinkCrystal = 1 + Math.floor(Math.random() * 12);
+console.log("pink crystal number " + pinkCrystal);
+
+var purpleCrystal = 1 + Math.floor(Math.random() * 12);
+console.log("purple crystal number" + purpleCrystal); 
+
+var orangeCrystal = 1 + Math.floor(Math.random() * 12);
+console.log("orange crystal number" + orangeCrystal); 
+
+var blackCrystal = 1 + Math.floor(Math.random() * 12);
+console.log("black crystal number" + blackCrystal); 
+
+$("#totalScore").html(userTotal);
+$("#wins").html(wins);
+$("#losses").html (losses);
+$("#numberToGuess").html(randomNumber);
+
+};
+
+
+//tells user if he won or lost
 var userWin = function() {
     if (userTotal === randomNumber){
     console.log("user wins!!");
+    wins ++;
+    $("#wins").html(wins);
+    reset();
     }
 
     else if (userTotal > randomNumber){
         console.log("you loose");
+        losses ++;
+        $("#losses").html (losses);
+        reset();
     }
 };
+
+
+
 
 
 //click function for crstals
@@ -38,6 +80,7 @@ $("#pinkCrystal").on("click", function(){
     console.log("this pink gem is working");
     userTotal = userTotal + pinkCrystal;
     console.log(userTotal);
+    $("#totalScore").html(userTotal);
     userWin ();
 })
 
@@ -45,6 +88,7 @@ $("#purpleCrystal").on("click", function(){
     console.log("this purple gem is working");
     userTotal = userTotal + purpleCrystal;
     console.log(userTotal);
+    $("#totalScore").html(userTotal);
     userWin ();
 })
 
@@ -52,6 +96,7 @@ $("#orangeCrystal").on("click", function(){
     console.log("this orange gem is working");
     userTotal = userTotal + orangeCrystal;
     console.log (userTotal);
+    $("#totalScore").html(userTotal);
     userWin ();
 })
 
@@ -59,5 +104,6 @@ $("#blackCrystal").on("click", function(){
     console.log("this black gem is working");
     userTotal = userTotal + blackCrystal;
     console.log(userTotal);
+    $("#totalScore").html(userTotal);
     userWin ();
 });
